f4ecb7610b0e0c1aa231c95d6d7fdb72e3bf457e
Bekzod X <Bekzod.2o1oX@gmail.com>
2025-03-25 16:14:07 +0500

    add file web app

--- sign.js
+++ sign.js
@@ -1,21 +1,19 @@
-let supbtn = document.querySelector(".signup_btn_click");
-let regiignsteremail = document.querySelector(".email-registr");
-let registerpassword = document.querySelector(".password-registr");
-let repeatpassword = document.querySelector(".repeat-password-registr");
+let supbtn = document.querySelector("#signupbtn");
 
-signupbtn.addEventListener("click", function () {
-    let email = registeremail.value.trim();
-    let password = registerpassword.value.trim();
-    let repeatPassword = repeatpassword.value.trim();
+supbtn.addEventListener("click", function () {
+    let email = document.getElementById("registeremail").value;
+    let password = document.getElementById("registerpassword").value;
+    let repeatPassword = document.getElementById("repeatpassword").value;
 
     if (email === "" || password === "" || repeatPassword === "") {
-        alert("❌ email va parol kiriting");
+        alert("❌ Email va parol kiriting");
     } else if (password.length < 6) {
-        alert("❌ Parol 6 ta belgidan kam bo'lmasin");
+        alert("❌ Parol 6 ta belgidan kam bo‘lmasin");
     } else if (password !== repeatPassword) {
         alert("❌ Parollar bir xil emas!");
     } else {
-        alert("Ro‘yxatdan o‘tish Yaxshi kechdi ✅");
+        alert("✅ Ro‘yxatdan o‘tdingiz");
         window.location.href = "index.html";
     }
 });
+
